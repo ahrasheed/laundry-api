73afd75942daf8a5e9f2f88bd64818ea87f2e342
ahrasheed <ahrasheed@gmail.com>
2015-01-19 03:02:44 +0500

    First Commit

---
+++ gae-boilerplate-master/api.py
@@ -0,0 +1,302 @@
+# from models import OrderStore, Order, OrderList, Item, ItemList, ItemStore
+# from google.appengine.ext import ndb
+
+# import endpoints
+# from protorpc import messages
+# from protorpc import message_types
+# from protorpc import remote
+
+
+
+# package = 'easy'
+
+# WEB_CLIENT_ID = 'replace this with your web client application ID'
+# ANDROID_CLIENT_ID = 'replace this with your Android client ID'
+# IOS_CLIENT_ID = 'replace this with your iOS client ID'
+# ANDROID_AUDIENCE = WEB_CLIENT_ID
+
+
+# @endpoints.api(name='laundry', version='v1',
+#                allowed_client_ids=[WEB_CLIENT_ID, ANDROID_CLIENT_ID,
+#                                    IOS_CLIENT_ID,
+#                                    endpoints.API_EXPLORER_CLIENT_ID],
+#                audiences=[ANDROID_AUDIENCE],
+#                scopes=[endpoints.EMAIL_SCOPE])
+# class LaundryApi(remote.Service):
+#     """Laundry API v1."""
+
+    # @endpoints.method(message_types.VoidMessage, OrderList,
+    #                   path='orders', http_method='GET',
+    #                   name='laundry.getAllOrders')
+    # def order_list(self, unused_request):
+
+    #     # if orderstorelist:
+    #     q = [o.to_message() for o in OrderStore.query().fetch()]
+    #     print q
+    #     o_list = OrderList(orderlist=q)
+    #     return o_list
+
+#     @endpoints.method(message_types.VoidMessage, ItemList,
+#                       path='items', http_method='GET',
+#                       name='laundry.getAllItems')
+#     def item_list(self, unused_request):
+#         q = [i.to_message() for i in ItemStore.query().fetch()]
+#         print q
+#         i_list = ItemList(itemlist=q)
+#         return i_list
+
+#     # ID_RESOURCE = endpoints.ResourceContainer(message_types.VoidMessage,
+#     #                                           id=messages.IntegerField(1, variant=messages.Variant.INT32))
+
+#     # @endpoints.method(ID_RESOURCE, Order,
+#     #                   path='orders/{id}', http_method='GET',
+#     #                   name='laundry.getOrder')
+#     # def order_get(self, request):
+
+#     #     q = OrderStore.query(OrderStore.order.orderid == request.id).get()
+#     #     if q is None:
+#     #         raise endpoints.NotFoundException('Order %s not found.' %
+#     #                                           (request.id,))
+#     #     else:
+#     #         return q.order
+
+#     # STATUS_RESOURCE = endpoints.ResourceContainer(
+#     #         StatusChange, id=messages.IntegerField(1, variant=messages.Variant.INT32))
+
+#     # @endpoints.method(STATUS_RESOURCE, message_types.VoidMessage,
+#     #                   path='orders/{id}', http_method='POST',
+#     #                   name='laundry.statusChange')
+#     # def status_change(self, request):
+#     #     o = OrderStore.query(OrderStore.order.orderid == request.id).get()
+#     #     o.order.status = request.status
+#     #     o.put()
+#     #     return message_types.VoidMessage()
+
+#     @endpoints.method(Order, message_types.VoidMessage,
+#                       path='placeorder', http_method='POST',
+#                       name='laundry.placeOrder')
+#     def place_order(self, request):
+#         if request.orderkey and request.status:
+#             if request.status == 'INCOMPLETE':
+#                 key = ndb.Key(OrderStore, request.orderkey)
+#                 o = key.get()
+#                 print o
+#                 o.update_from_message(request)
+#                 return message_types.VoidMessage()
+#         o = OrderStore.put_from_message(request)
+#         if o:
+#             return message_types.VoidMessage()
+
+#     @endpoints.method(Item, message_types.VoidMessage,
+#                       path='additem', http_method='POST',
+#                       name='laundry.addItem')
+#     def add_item(self, request):
+#         i = ItemStore.put_from_message(request)
+#         if i:
+#             return message_types.VoidMessage()
+
+import Cookie
+import logging
+import endpoints
+import os
+from google.appengine.ext import ndb
+from protorpc import remote
+from protorpc import message_types
+from protorpc import messages
+import time
+from webapp2_extras.sessions import SessionDict
+from bp_includes.models import User
+from webapp2_extras import sessions, securecookie, auth
+from bp_content.themes.default.handlers.models import OrderStore, Order, OrderList, Item, ItemList, ItemStore
+
+
+package = 'easy'
+
+TOKEN_CONFIG = {
+    'token_max_age': 86400 * 7 * 3,
+    'token_new_age': 86400,
+    'token_cache_age': 3600,
+}
+
+SESSION_ATTRIBUTES = ['user_id', 'remember',
+                      'token', 'token_ts', 'cache_ts']
+
+SESSION_SECRET_KEY = '9C3155EFEEB9D9A66A22EDC16AEDA'
+
+class TestMessage(messages.Message):
+    username = messages.StringField(2)
+
+
+@endpoints.api(name='laundry_api', version='v1',
+               description='Laundry API')
+class LaundryApi(remote.Service):
+    user = None
+    token = None
+
+    @classmethod
+    def get_user_from_cookie(cls):
+        serializer = securecookie.SecureCookieSerializer(SESSION_SECRET_KEY)
+        cookie_string = os.environ.get('HTTP_COOKIE')
+        cookie = Cookie.SimpleCookie()
+        cookie.load(cookie_string)
+        session = cookie['session'].value
+        session_name = cookie['session_name'].value
+        session_name_data = serializer.deserialize('session_name', session_name)
+        session_dict = SessionDict(cls, data=session_name_data, new=False)
+
+        if session_dict:
+            session_final = dict(zip(SESSION_ATTRIBUTES, session_dict.get('_user')))
+            _user, _token = cls.validate_token(session_final.get('user_id'), session_final.get('token'),
+                                               token_ts=session_final.get('token_ts'))
+            cls.user = _user
+            cls.token = _token
+
+    @classmethod
+    def user_to_dict(cls, user):
+        """Returns a dictionary based on a user object.
+
+        Extra attributes to be retrieved must be set in this module's
+        configuration.
+
+        :param user:
+            User object: an instance the custom user model.
+        :returns:
+            A dictionary with user data.
+        """
+        if not user:
+            return None
+
+        user_dict = dict((a, getattr(user, a)) for a in [])
+        user_dict['user_id'] = user.get_id()
+        return user_dict
+
+    @classmethod
+    def get_user_by_auth_token(cls, user_id, token):
+        """Returns a user dict based on user_id and auth token.
+
+        :param user_id:
+            User id.
+        :param token:
+            Authentication token.
+        :returns:
+            A tuple ``(user_dict, token_timestamp)``. Both values can be None.
+            The token timestamp will be None if the user is invalid or it
+            is valid but the token requires renewal.
+        """
+        user, ts = User.get_by_auth_token(user_id, token)
+        return cls.user_to_dict(user), ts
+
+    @classmethod
+    def validate_token(cls, user_id, token, token_ts=None):
+        """Validates a token.
+
+        Tokens are random strings used to authenticate temporarily. They are
+        used to validate sessions or service requests.
+
+        :param user_id:
+            User id.
+        :param token:
+            Token to be checked.
+        :param token_ts:
+            Optional token timestamp used to pre-validate the token age.
+        :returns:
+            A tuple ``(user_dict, token)``.
+        """
+        now = int(time.time())
+        delete = token_ts and ((now - token_ts) > TOKEN_CONFIG['token_max_age'])
+        create = False
+
+        if not delete:
+            # Try to fetch the user.
+            user, ts = cls.get_user_by_auth_token(user_id, token)
+            if user:
+                # Now validate the real timestamp.
+                delete = (now - ts) > TOKEN_CONFIG['token_max_age']
+                create = (now - ts) > TOKEN_CONFIG['token_new_age']
+
+        if delete or create or not user:
+            if delete or create:
+                # Delete token from db.
+                User.delete_auth_token(user_id, token)
+
+                if delete:
+                    user = None
+
+            token = None
+
+        return user, token
+
+    @endpoints.method(message_types.VoidMessage, OrderList,
+                      path='orders', http_method='GET',
+                      name='laundry.getAllOrders')
+    def order_list(self, request):
+
+            # if orderstorelist:
+        self.get_user_from_cookie()
+
+        if not self.user:
+            raise endpoints.UnauthorizedException('Invalid token.')
+
+        userkey_id = self.user['user_id']
+        user_key = ndb.Key('User', userkey_id)
+        current_user = user_key.get()
+        print current_user.username
+
+        q = [o.to_message() for o in OrderStore.query(OrderStore.user.username == current_user.username).fetch()]
+        print q
+        o_list = OrderList(orderlist=q)
+        return o_list
+
+    @endpoints.method(message_types.VoidMessage, ItemList,
+                      path='items', http_method='GET',
+                      name='laundry.getAllItems')
+    def item_list(self, unused_request):
+        q = [i.to_message() for i in ItemStore.query().fetch()]
+        print q
+        i_list = ItemList(itemlist=q)
+        return i_list
+
+# ---- POST METHODS-----#
+
+    @endpoints.method(Item, message_types.VoidMessage,
+                      path='additem', http_method='POST',
+                      name='laundry.addItem')
+    def add_item(self, request):
+        i = ItemStore.put_from_message(request)
+        if i:
+            return message_types.VoidMessage()
+
+    @endpoints.method(Order, message_types.VoidMessage,
+                      path='placeorder', http_method='POST',
+                      name='laundry.placeOrder')
+    def place_order(self, request):
+        # if request.orderkey and request.status:
+        #     if request.status == 'INCOMPLETE':
+        #         key = ndb.Key(OrderStore, request.orderkey)
+        #         o = key.get()
+        #         print o
+        #         o.update_from_message(request)
+        #         return message_types.VoidMessage()
+        o = OrderStore.put_from_message(request)
+        if o:
+            return message_types.VoidMessage()
+
+
+    @endpoints.method(message_types.VoidMessage, TestMessage,
+                      path='testing', http_method='GET',
+                      name='testit')
+    def testit(self, request):
+        # return Item(username='testit')
+        self.get_user_from_cookie()
+
+        if not self.user:
+            raise endpoints.UnauthorizedException('Invalid token.')
+
+        userkey_id = self.user['user_id']
+        user_key = ndb.Key('User', userkey_id)
+        user = user_key.get()
+
+        return TestMessage(username=user.username)
+
+
+APPLICATION = endpoints.api_server([LaundryApi], restricted=False)
---
+++ gae-boilerplate-master/bp_content/themes/default/handlers/models.py
@@ -0,0 +1,190 @@
+from google.appengine.ext import ndb
+
+from protorpc import messages
+from protorpc import message_types
+from bp_includes.models import User
+
+# ----- MESSAGES ------
+
+
+class Item(messages.Message):
+    """Individual clothing items"""
+    itemkey = messages.IntegerField(1)
+    name = messages.StringField(2)
+    weight = messages.FloatField(3)
+    rate = messages.FloatField(4)
+
+
+class ItemCount(messages.Message):
+    """Individual clothing items"""
+    itemkey = messages.IntegerField(1)
+    name = messages.StringField(2)
+    number = messages.IntegerField(3, default=1, required=True)
+    weight = messages.FloatField(4)
+    rate = messages.FloatField(5)
+
+
+class Order(messages.Message):
+    """Collection of clothes ordered by a user"""
+    orderkey = messages.IntegerField(1)
+    username = messages.StringField(2)
+    items = messages.MessageField(ItemCount, 3, repeated=True)
+    pickuplocation = messages.StringField(4)
+    pickuptime = message_types.DateTimeField(5)
+    droplocation = messages.StringField(6)
+    droptime = message_types.DateTimeField(7)
+    orderplacedtime = message_types.DateTimeField(8)
+    status = messages.StringField(9)
+    comments = messages.StringField(10)
+
+
+# class StatusChange(messages.Message):
+#     """Status change message"""
+#     status = messages.EnumField(OrderStatus, 1, required=True)
+
+
+class ItemList(messages.Message):
+    """Item list"""
+    itemlist = messages.MessageField(Item, 1, repeated=True)
+
+
+class OrderList(messages.Message):
+    """Order list for user"""
+    orderlist = messages.MessageField(Order, 1, repeated=True)
+    userid = messages.IntegerField(2)
+
+
+# ----- DATE STORE MODELS ------
+
+class ItemCountStore(ndb.Model):
+    """Structured Property for Orders"""
+    name = ndb.StringProperty()
+    number = ndb.IntegerProperty(default=1)
+    weight = ndb.FloatProperty()
+    rate = ndb.FloatProperty()
+
+
+class ItemStore(ndb.Model):
+    """Save Items"""
+    name = ndb.StringProperty()
+    weight = ndb.FloatProperty()
+    rate = ndb.FloatProperty()
+
+    def to_message(self):
+        """Turns the Order into a message"""
+
+        item = Item(itemkey=self.key.id(),
+                    name=self.name,
+                    weight=self.weight,
+                    rate=self.rate
+                    )
+        return item
+
+    @classmethod
+    def put_from_message(cls, message):
+        """Insert order from message."""
+
+        item = cls(name=message.name,
+                   weight=message.weight,
+                   rate=message.rate
+                   )
+        item.put()
+
+        return item
+
+
+class OrderStore(ndb.Model):
+    """Save Orders"""
+    user = ndb.StructuredProperty(User)
+    items = ndb.StructuredProperty(ItemCountStore, repeated=True)
+    pickuplocation = ndb.StringProperty()
+    pickuptime = ndb.DateTimeProperty()
+    droplocation = ndb.StringProperty()
+    droptime = ndb.DateTimeProperty()
+    orderplacedtime = ndb.DateTimeProperty()
+    status = ndb.StringProperty(default='INCOMPLETE', choices=['INCOMPLETE', 'INIT', 'PROCESSING', 'ENROUTE', 'COMPLETED'])
+    comments = ndb.TextProperty()
+
+    @property
+    def orderweight(self):
+        """Property to calculate total weight"""
+        total = 0
+        for item in self.items:
+            total += item.weight
+        return total
+
+    def to_message(self):
+        """Turns the Order into a message"""
+        itemsie = [ItemCount(name=item.name, number=item.number, weight=item.weight, rate=item.rate) for item in self.items]
+
+        order = Order(orderkey=self.key.id(),
+                      username=self.user.username,
+                      items=itemsie,
+                      pickuplocation=self.pickuplocation,
+                      pickuptime=self.pickuptime,
+                      droplocation=self.droplocation,
+                      droptime=self.droptime,
+                      orderplacedtime=self.orderplacedtime,
+                      status=self.status,
+                      comments=self.comments
+                      )
+        return order
+
+    def update_status(self, status):
+        """Update order stataus"""
+        # add checks for completion here if necessary
+        self.status = status
+
+    def update_from_message(self, message):
+        """Update incomplete orders"""
+        if message.items:
+            itemsie = []
+            for item in message.items:
+                i = ItemStore.query(ItemStore.itemid == item.itemid).get()
+                if i:
+                    itemsie.append(ItemCountStore(name=i.name, number=item.number, weight=i.weight, rate=i.rate))
+            self.items = itemsie
+
+        if message.pickuplocation:
+            self.pickuplocation = message.pickuplocation
+
+        if message.pickuptime:
+            self.pickuptime = message.pickuptime
+
+        if message.droplocation:
+            self.droplocation = message.droplocation
+
+        if message.droptime:
+            self.droptime = message.droptime
+
+        if message.orderplacedtime:
+            self.orderplacedtime = message.orderplacedtime
+
+        if message.comments:
+            self.comments = message.comments
+
+    @classmethod
+    def put_from_message(cls, message):
+        """Insert order from message."""
+        itemsie = []
+        if message.items:
+            for item in message.items:
+                print item.itemkey
+                i_key = ndb.Key('ItemStore', item.itemkey)
+                i = i_key.get()
+                if i:
+                    itemsie.append(ItemCountStore(name=i.name, number=item.number, weight=i.weight, rate=i.rate))
+        user = User.query(User.username == message.username).fetch()[0]
+        order = cls(user=user,
+                    items=itemsie,
+                    pickuplocation=message.pickuplocation,
+                    pickuptime=message.pickuptime,
+                    droplocation=message.droplocation,
+                    droptime=message.droptime,
+                    orderplacedtime=message.orderplacedtime,
+                    status=message.status,
+                    comments=message.comments
+                    )
+        order.put()
+        return order
+
